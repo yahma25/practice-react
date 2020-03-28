60894319f7fea384f0885272ac9425340ff7a992
mhkim <yahma25@gmail.com>
2020-03-28 18:47:18 +0900

    ModalProvider 추가

--- src/stories/ModalStory.jsx
+++ src/stories/ModalStory.jsx
@@ -5,6 +5,7 @@ import Modal from '../06/Modal';
 import Button from '../04/Button';
 import Text from '../04/Text';
 import ButtonWithModal from '../06/ButtonWithModal';
+import ModalProvider, { Consumer } from '../06/ModalProvider';
 
 storiesOf('Modal', module)
   .addWithJSX('기본 설정', () => (
@@ -16,4 +17,12 @@ storiesOf('Modal', module)
       <Button>닫기</Button>
     </Modal>
   ))
-  .addWithJSX('ButtonWithModal', () => <ButtonWithModal />);
+  .addWithJSX('ButtonWithModal', () => <ButtonWithModal />)
+  .addWithJSX('ModalProvider', () => (
+    <ModalProvider>
+      <div>
+        <Text>다음 버튼을 눌러 모달을 실행합니다.</Text>
+        <Consumer>{({ openModal }) => <Button onPress={() => openModal()}>삭제</Button>}</Consumer>
+      </div>
+    </ModalProvider>
+  ));
