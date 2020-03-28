0af4cc3dbcfd798f8c0b125e17534bad6ed0dc6f
mhkim <yahma25@gmail.com>
2020-03-28 18:33:39 +0900

    Modal이 열리는 버튼 추가

--- src/stories/ModalStory.jsx
+++ src/stories/ModalStory.jsx
@@ -4,13 +4,16 @@ import { storiesOf } from '@storybook/react';
 import Modal from '../06/Modal';
 import Button from '../04/Button';
 import Text from '../04/Text';
+import ButtonWithModal from '../06/ButtonWithModal';
 
-storiesOf('Modal', module).addWithJSX('기본 설정', () => (
-  <Modal>
-    <div>
-      <Text>정말로 삭제하시겠습니까?</Text>
-    </div>
-    <Button primary>예</Button>
-    <Button>닫기</Button>
-  </Modal>
-));
+storiesOf('Modal', module)
+  .addWithJSX('기본 설정', () => (
+    <Modal>
+      <div>
+        <Text>정말로 삭제하시겠습니까?</Text>
+      </div>
+      <Button primary>예</Button>
+      <Button>닫기</Button>
+    </Modal>
+  ))
+  .addWithJSX('ButtonWithModal', () => <ButtonWithModal />);
