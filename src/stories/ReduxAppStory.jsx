444757f635ad6d67b7dad56b9fcb8aec8d070069
mhkim <yahma25@gmail.com>
2020-04-04 17:18:24 +0900

    Redux 기본 사용

---
+++ src/stories/ReduxAppStory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ReduxApp from '../07/ReduxApp01';
+
+storiesOf('ReduxApp', module).addWithJSX('기본 예제', () => <ReduxApp />);
