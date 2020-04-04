bd2eb5f3dd96c4d758471a1c5e2c63487bcbf9b9
mhkim <yahma25@gmail.com>
2020-04-04 17:23:45 +0900

    action 호출

--- src/stories/ReduxAppStory.jsx
+++ src/stories/ReduxAppStory.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import ReduxApp from '../07/ReduxApp01';
+// import ReduxApp from '../07/ReduxApp01';
+import ReduxApp from '../07/ReduxApp02';
 
 storiesOf('ReduxApp', module).addWithJSX('기본 예제', () => <ReduxApp />);
