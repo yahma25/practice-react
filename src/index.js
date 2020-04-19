89e9a083823878e84c3e90bbd6fdaf63e7fb5c9e
mhkim <yahma25@gmail.com>
2020-04-19 23:49:11 +0900

    코인 거래소 샘플 프로젝트 따라하기 - 상단 네비게이션 영역 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './doit-ui/app.css';
+import CoinApp from './08/CoinApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CoinApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
