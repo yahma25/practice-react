e16cb69ad0ae04bd92784dd5073b4caec0515185
mhkim <yahma25@gmail.com>
2020-05-03 20:02:53 +0900

    메인 화면 구성

--- src/08/CoinApp.jsx
+++ src/08/CoinApp.jsx
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react';
 import AppLayout from './components/AppLayout';
+import MainPage from './components/main/MainPage';
 
 class CoinApp extends PureComponent {
   render() {
-    return <AppLayout>본문</AppLayout>;
+    return (
+      <AppLayout>
+        <MainPage />
+      </AppLayout>
+    );
   }
 }
 
