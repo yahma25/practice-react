b957d4ffe7269b2040839e45fa74319da8a120de
mhkim <yahma25@gmail.com>
2020-05-11 22:15:24 +0900

    매수 기능을 redux-pack을 사용하는 것으로 교체

--- src/08/actions/transactionPackActions.js
+++ src/08/actions/transactionPackActions.js
@@ -1,6 +1,17 @@
 import Api from '../Api';
 
 export const FETCH_TRANSACTION_LIST = 'transaction/FETCH_TRANSACTION_LIST';
+export const CREATE_TRANSACTION = 'transaction/CREATE_TRANSACTION';
+
+export function createTransaction(data, onComplete) {
+  return {
+    type: CREATE_TRANSACTION,
+    promise: Api.post('/transactions', data),
+    meta: {
+      onSuccess: onComplete
+    }
+  };
+}
 
 export function requestTransactionList(params) {
   return {
