bb7d84b6beffbcf998361ba5537e1fc8013d391a
mhkim <yahma25@gmail.com>
2020-05-11 21:53:14 +0900

    redux-pack을 사용하여 비동기적으로 코인 리스트 가져오는 API 변경

--- src/08/reducers/transactionsReducer.js
+++ src/08/reducers/transactionsReducer.js
@@ -1,4 +1,6 @@
 import { LOADING_TRANSACTION_LIST, SET_ERROR, SET_TRANSACTION_LIST } from '../actions/transactionActions';
+import {handle} from 'redux-pack';
+import {FETCH_TRANSACTION_LIST} from '../actions/transactionPackActions';
 
 const initState = {
   ids: [],
@@ -11,29 +13,65 @@ export default (state = initState, action) => {
   const {type, payload} = action;
 
   switch (type) {
-    case SET_ERROR: {
-      return {
-        ...state,
-        loading: false,
-        hasError: true,
-        errorMessage: payload.errorMessage
-      }
+    case FETCH_TRANSACTION_LIST: {
+      return handle(state, action, {
+        start: prevState => ({
+          ...prevState,
+          loading: true,
+          hasError: false
+        }),
+        success: prevState => {
+          const {data} = payload;
+          const ids = data.map(entity => entity['id']);
+          const entities = data.reduce(
+            (finalEntities, entity) => ({
+              ...finalEntities,
+              [entity['id']]: entity
+            }),
+            {}
+          );
+          return {
+            ...prevState,
+            ids,
+            entities,
+            loading: false,
+            hasError: false
+          };
+        },
+        failure: prevState => {
+          const {errorMessage} = payload.response.data;
+          return {
+            ...prevState,
+            loading: false,
+            hasError: true,
+            errorMessage
+          };
+        }
+      });
     }
-    case LOADING_TRANSACTION_LIST: {
-      return {
-        ...state,
-        loading: true,
-        hasError: false
-      }
-    }
-    case SET_TRANSACTION_LIST: {
-      const ids = payload.map(entity => entity['id']);
-      const entities = payload.reduce((finalEntities, entity) => ({
-        ...finalEntities,
-        [entity['id']]: entity
-      }), {});
-      return {...state, ids, entities, loading: false, hasError: false};
-    }
+    // case SET_ERROR: {
+    //   return {
+    //     ...state,
+    //     loading: false,
+    //     hasError: true,
+    //     errorMessage: payload.errorMessage
+    //   }
+    // }
+    // case LOADING_TRANSACTION_LIST: {
+    //   return {
+    //     ...state,
+    //     loading: true,
+    //     hasError: false
+    //   }
+    // }
+    // case SET_TRANSACTION_LIST: {
+    //   const ids = payload.map(entity => entity['id']);
+    //   const entities = payload.reduce((finalEntities, entity) => ({
+    //     ...finalEntities,
+    //     [entity['id']]: entity
+    //   }), {});
+    //   return {...state, ids, entities, loading: false, hasError: false};
+    // }
     default:
       return state;
   }
