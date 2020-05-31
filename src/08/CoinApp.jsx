921e125366163f51d429f72009e2679db93b58db
mhkim <yahma25@gmail.com>
2020-05-31 18:41:54 +0900

    Router를 활용하여 메인 페이지와 잘못된 경로의 페이지 안내를 보여주도록 하는 기능 추가

--- src/08/CoinApp.jsx
+++ src/08/CoinApp.jsx
@@ -3,22 +3,28 @@ import { Provider } from 'react-redux';
 import AppLayout from './components/AppLayout';
 import configureStore from './store/configureStore';
 import ModalProvider from './ModalProvider';
-import CoinOverview from './components/main/CoinOverview';
-import TransactionListContainer from './containers/main/TransactionListContainer';
 import NotificationContainer from './containers/NotificationContainer';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from './components/main/MainPage';
+import NotFound from './components/NotFound';
 
 class CoinApp extends PureComponent {
   store = configureStore();
   render() {
     return (
       <Provider store={this.store}>
-        <ModalProvider>
-          <AppLayout>
-            <CoinOverview />
-            <TransactionListContainer />
-            <NotificationContainer />
-          </AppLayout>
-        </ModalProvider>
+        <Router>
+          <ModalProvider>
+            <AppLayout>
+              <Switch>
+                <Route path="/" exact render={() => <MainPage />} />
+                <Route path="*" component={NotFound} />
+              </Switch>
+              <NotificationContainer />
+            </AppLayout>
+          </ModalProvider>
+        </Router>
       </Provider>
     );
   }
---
+++ src/08/components/NotFound.jsx
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+
+import Text from '../../doit-ui/Text';
+import Spacing from '../../doit-ui/Spacing';
+
+class NotFound extends PureComponent {
+  render() {
+    const { url } = this.props.match || {};
+    return (
+      <>
+        <Spacing bottom={2}>
+          <Text xlarge>{url} 페이지를 찾을 수 없습니다.</Text>
+        </Spacing>
+        <Link to="/">메인 페이지로 이동</Link>
+      </>
+    );
+  }
+}
+
+export default NotFound;
