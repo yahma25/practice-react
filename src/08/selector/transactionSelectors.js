4f2a9f03736bf65e8672a783691fff061e524969
mhkim <yahma25@gmail.com>
2020-05-31 15:34:56 +0900

    selector를 한 번에 묶어서 제공하는 것으로 변경

--- src/08/selector/transactionSelectors.js
+++ src/08/selector/transactionSelectors.js
@@ -1,14 +1,9 @@
-import { FETCH_TRANSACTION_LIST, CREATE_TRANSACTION } from '../actions/transactionPackActions';
-import { createSelector } from 'reselect';
+import createSelectors from '../../11/api-redux-pack/createSelectors';
 
-export const transactionsSelector = state => state.transactions;
-export const transactionListSelector = createSelector(
-  [transactionsSelector],
-  transactions => {
-    const { ids, entities } = transactions;
-    return ids.map(id => entities[id])
-  }
-);
-export const loadingStateSelector = state => transactionsSelector(state).loadingState;
-export const transactionListLoadingStateSelector = state => loadingStateSelector(state)[FETCH_TRANSACTION_LIST];
-export const transactionCreateLoadingStateSelector = state => loadingStateSelector(state)[CREATE_TRANSACTION];
+export const {
+  resourceSelector: transactionSelector,
+  collectionSelector: transactionListSelector,
+  collectionLoadingStateSelector: transactionListLoadingStateSelector,
+  createLoadingStateSelector: transactionCreateLoadingStateSelector,
+  paginationSelector
+} = createSelectors('transactions');
