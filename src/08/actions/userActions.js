0213bd8b5a3e38f884e706a51ab93e4b3d409d66
mhkim <yahma25@gmail.com>
2020-05-31 17:06:22 +0900

    회원 가입 버튼과 모달 폼 추가

---
+++ src/08/actions/userActions.js
@@ -0,0 +1,13 @@
+import createActions from '../../11/api-redux-pack/createActions';
+
+const { create } = createActions('uses');
+export function createUser(data, onComplete) {
+  return create(
+    data,
+    {},
+    {
+      notification: { success: '회원 가입이 성공적으로 완료되었습니다.' },
+      onSuccess: onComplete
+    }
+  );
+}
--- src/08/components/AppNav.jsx
+++ src/08/components/AppNav.jsx
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
 import { withStyles, css, withStylesPropTypes } from '../../doit-ui/withStyles';
 import Heading from '../../doit-ui/Heading';
-import Text from '../../doit-ui/Text';
+import { Consumer as Modal } from '../../doit-ui/Modal/context';
+import Button from '../../doit-ui/Button';
+import { REGISTER_USER_MODAL } from '../constants/modals';
 
 export const HEIGHT = 64;
 
@@ -14,9 +16,13 @@ class AppNav extends PureComponent {
           <Heading level={3} inverse>
             두잇 코인 거래소
           </Heading>
-          <Text inverse bold large>
-            회원가입
-          </Text>
+          <Modal>
+            {({ openModal }) => (
+              <Button inverse bold large onPress={() => openModal(REGISTER_USER_MODAL)}>
+                회원 가입
+              </Button>
+            )}
+          </Modal>
         </div>
       </div>
     );
